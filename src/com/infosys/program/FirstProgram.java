10b47767b6326939680c7ad467b21608c67734cb
neha98chavan <neha98chavan@gmail.com>
2024-06-20 20:09:36 +1200

    added

---
+++ src/com/infosys/program/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.infosys.program;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!!.....");
+		System.out.println(10+20);
+
+	}
+
+}
